a3163eca6e14f19ddd9dd4d325b9a67f445fc659
cori <tiziano313rivadeneira@gmail.com>
2026-03-25 23:53:37 -0300

    added ESlint dependencies + new config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,31 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import jestPlugin from "eslint-plugin-jest";
+import globals from "globals";
+import prettierConfig from "eslint-config-prettier";
 
-export default defineConfig([
+export default [
+  // 1. Base JS rules
+  js.configs.recommended,
+  
+  // 2. Your custom Jest config
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "**/__tests__/**/*"],
     plugins: {
-      js,
+      jest: jestPlugin,
     },
-    extends: ["js/recommended", "prettier"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
     rules: {
+      ...jestPlugin.configs.recommended.rules,
       "no-unused-vars": "warn",
       "no-undef": "warn",
     },
   },
-]);
+  
+  // 3. Prettier config (must be last to disable conflicting rules)
+  prettierConfig,
+];
